e576bcc337ca0757b2bd58e3c625964368da599a
Andrew Zak <azak@hey.com>
2023-03-25 14:16:54 +0000

    Login page is done

--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
     styleUrls: ['login.page.scss']
 })
 export class LoginPage {
-    constructor() {}
+    constructor(
+        private router: Router
+    ) {}
 
+    public login() {
+
+    }
 }
